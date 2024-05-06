cc0095d8bdc21e403de0b00a595d46f76fcb498b
lilygannon <lrg180000@utdallas.edu>
2024-05-05 20:49:01 -0500

    stuff

--- src/RainfallTest.py
+++ src/RainfallTest.py
@@ -1,59 +1,63 @@
 import pygame
 import random
 
+WIDTH = 1080
+HEIGHT = 580
+
+
 class Rain(pygame.sprite.Sprite):
-    def __init__(self):
+    def __init__(self, rain_img):
         pygame.sprite.Sprite.__init__(self)
         self.image = rain_img
         self.rect = self.image.get_rect()
         self.speedx = 3
         self.speedy = random.randint(5, 25)
-        self.rect.x = random.randint(-100, wn_width)
-        self.rect.y = random.randint(-wn_height, -5)
+        self.rect.x = random.randint(-100, WIDTH)
+        self.rect.y = random.randint(-HEIGHT, -5)
 
     def update(self):
 
-        if self.rect.bottom > wn_height:
+        if self.rect.bottom > HEIGHT:
             self.speedx = 3
             self.speedy = random.randint(5, 25)
-            self.rect.x = random.randint(-wn_width, wn_width)
-            self.rect.y = random.randint(-wn_height, -5)
+            self.rect.x = random.randint(-HEIGHT, WIDTH)
+            self.rect.y = random.randint(-HEIGHT, -5)
 
         self.rect.x = self.rect.x + self.speedx
         self.rect.y = self.rect.y + self.speedy
 
 
-pygame.init()
+def main():
+    pygame.init()
 
-clock = pygame.time.Clock()
+    clock = pygame.time.Clock()
 
-bg_color = (52, 69, 76)
+    bg_color = (52, 69, 76)
 
-rain_img = pygame.image.load('src/Raindrop.png')
+    rain_img = pygame.image.load('src/Raindrop.png')
 
-wn_width = 700
-wn_height = 500
-wn = pygame.display.set_mode((wn_width, wn_height))
-pygame.display.set_caption('Torrential Rain')
+    wn = pygame.display.set_mode((WIDTH, HEIGHT))
+    pygame.display.set_caption('Torrential Rain')
 
-rain_group = pygame.sprite.Group()
+    rain_group = pygame.sprite.Group()
 
-for i in range(100):
-    rain = Rain()
-    rain_group.add(rain)
+    for i in range(100):
+        rain = Rain(rain_img)
+        rain_group.add(rain)
 
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            quit()
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
 
-    rain_group.update()
+        rain_group.update()
 
-    wn.fill(bg_color)
-    rain_group.draw(wn)
-    pygame.display.flip()
-    clock.tick(30)
+        wn.fill(bg_color)
+        rain_group.draw(wn)
+        pygame.display.flip()
+        clock.tick(30)
 
-pygame.quit()
-quit()
+
+if __name__ == "__main__":
+    main()
